8f8fb129b1c53b6e4957ac50d6cb62102a3ab70c
Eduardo Nadai <141272250+dueduno@users.noreply.github.com>
2026-03-23 09:17:26 -0300

    Update FieldLabel.tsx
    apenas comentários

--- apps/editor/src/components/FieldLabel.tsx
+++ apps/editor/src/components/FieldLabel.tsx
@@ -1,8 +1,17 @@
+// Criado a principio para servir de base ao tooltipicon (para inserir info dentro do TextInput)
+// Componente aux que monta o label de um campo de formulário com o icone i ao lado
+
+// Pelo que pesquisei, o campo do mantine TextInput aceita qualquer ReactNode na prop do label; 
+
+// O componente FieldLabel faz com que consiga ser um "label composto de mais de 1 item":
+// na pratica, o FieldLabel transforma o label normal: " TITLE ", em: " TITLE     i ", onde i é o tooltipicon
+
+
 import { Group, Text } from "@mantine/core";
 import TooltipIcon from "./TooltipIcon";
 import { ReactNode } from "react";
 
-// Helper que monta o label com o ℹ inline
+// helper que monta o label com o ℹ inline
 export function FieldLabel({ text, tooltip }: { text: string; tooltip: ReactNode }) {
   return (
     <Group gap={4} wrap="nowrap">
